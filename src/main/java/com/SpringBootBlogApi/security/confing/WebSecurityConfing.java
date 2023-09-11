b0100732db836d8de746ff1f249cf1bd61ab9372
Erdoğan Mücahit Alımcı <mcht.almc@gmail.com>
2023-09-11 13:38:37 +0300

    EndSecurity

--- src/main/java/com/SpringBootBlogApi/security/confing/WebSecurityConfing.java
+++ src/main/java/com/SpringBootBlogApi/security/confing/WebSecurityConfing.java
@@ -1,21 +1,35 @@
 package com.SpringBootBlogApi.security.confing;
 
+import com.SpringBootBlogApi.security.jwt.AuthEntryPointJwt;
+import com.SpringBootBlogApi.security.jwt.AuthTokenFilter;
+import com.SpringBootBlogApi.security.service.UserDetailServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@RequiredArgsConstructor
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
 public class WebSecurityConfing {
+    private final UserDetailServiceImpl userDetailService;
+    private final AuthEntryPointJwt unauthorizedHandler;
+
+
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
@@ -24,19 +38,57 @@ public class WebSecurityConfing {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.cors().
-                and().
-                csrf().
-                disable()
-                //TODO:
+
+        http.cors().and()
+                .csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .authorizeRequests().antMatchers().permitAll()
+                .authorizeRequests().antMatchers(AUTH_WHITE_LIST).permitAll()
                 .anyRequest().authenticated();
 
         http.headers().frameOptions().sameOrigin();
-        //TODO:
-        http.authenticationProvider();
+        http.authenticationProvider(authenticationProvider());
+        http.addFilterBefore(authTokenJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
+
+
+
+    @Bean
+    public AuthTokenFilter authTokenJwtTokenFilter(){
+        return new AuthTokenFilter();
+    }
+
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider= new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedHeaders("*")
+                        .allowedMethods("*");
+            }
+        };
+    }
+    private static final String[] AUTH_WHITE_LIST={
+      "/",
+      "index.html",
+      "/images/**",
+            "/css/**",
+            "/js/**",
+
+    };
 
 }
